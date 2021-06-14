2ef5bd9c76f2012d39db5199986cc1ba061e8821
khuara17 <vishalkamlapure.be@gmail.com>
2021-06-14 13:43:21 +0530

    14 jun

---
+++ variables.py
@@ -0,0 +1,51 @@
+######## Comments ########
+# Download git on computer and execute commanda on CMD/Terminal
+# git clone https://github.com/khuara17/Python-Fundamentals.git
+# git fecth 
+
+""" dkfjkdf
+jfhjkd
+ahsdfjdh  """
+''' this is first line
+this is second line
+this is third line '''
+
+# print("hello world") # I am printing helllo world.
+
+
+########## Variable  ####################
+
+''' Variables are to store data '''
+
+'''
+A variable name must start with a letter or the underscore character
+A variable name cannot start with a number
+A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+Variable names are case-sensitive (age, Age and AGE are three different variables)'''
+
+# Can start with: a-z, A - z , _
+# Cannot start with : 0-9
+# Can Contain : A-Z, a-z, 0-9, _
+# Cannot Contain : @, $ , % ,& 
+
+myglass = 4
+Myglass = 5
+myglasS = 4
+
+
+print(myglass) 
+
+Container = 4
+Container2_ = 6
+_glass22@ = 6
+
+######## Datatypes ##########
+
+## Integer, float , string 
+
+# Integer -: 0 - infinity -- real numbers
+# float :- 2.3 , 4.000 -- Decimal number
+# String :- "water567" or 'juice@#_' - Enclosed in qoutes
+
+
+
