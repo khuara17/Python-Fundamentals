6409a56c2de92219102f983141a8eeed6f89670c
khuara17 <vishalkamlapure.be@gmail.com>
2021-06-15 13:39:22 +0530

    15 jun

---
+++ Strings.py
@@ -0,0 +1,48 @@
+##### Multingle Strings #########
+
+mystring = ''' this is my first program
+ I love programming 
+ I love python ''' 
+
+m = "I love python" 
+
+# print(m[7])
+
+##### len()   ########
+ 
+# x = len(m)
+# print(x)
+
+######### String Slicing ###########
+
+# If accessing single index and out of range :- error
+# If accessing range(slicing) and out of range :- It will go till last index and stop.
+p = "banana"
+
+x = p[1:8]
+
+# print(x)
+
+######## String Formatting #######
+
+# Method 1
+age = 28
+friendage = 24
+myage = "My age is {} and my friend age is {}".format(12,34)
+# x = myage.
+
+print(myage)
+
+
+# Method 2
+quantity = 3
+itemno = 567
+price = 49.95
+# myorder = "I want to pay {1} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity, itemno, price)) 
+
+# Method 3
+
+# x = 
+
+print(f"I want to pay {'mystr****'} dollars for {itemno} pieces of item {price}")
--- variables.py
+++ variables.py
@@ -28,16 +28,16 @@ Variable names are case-sensitive (age, Age and AGE are three different variable
 # Can Contain : A-Z, a-z, 0-9, _
 # Cannot Contain : @, $ , % ,& 
 
-myglass = 4
-Myglass = 5
-myglasS = 4
+# myglass = 4
+# Myglass = 5
+# myglasS = 4
 
 
-print(myglass) 
+# print(myglass) 
 
-Container = 4
-Container2_ = 6
-_glass22@ = 6
+# Container = 4
+# Container2_ = 6
+# _glass22@ = 6
 
 ######## Datatypes ##########
 
@@ -46,6 +46,47 @@ _glass22@ = 6
 # Integer -: 0 - infinity -- real numbers
 # float :- 2.3 , 4.000 -- Decimal number
 # String :- "water567" or 'juice@#_' - Enclosed in qoutes
+
+
+###### Type Casting #######
+
+#Type()
+
+# print(type(v))
+
+
+
+# int == float and vice versa
+# int or float --> str not vice versa
+
+# x = 3
+# y = 2.3
+# z = int(2.3)
+# w = float(60)
+# i = str(3.3) 
+# v = float('A')   
+# j = 'hello'
+# print(v)
+
+#### Assign Multiple Values #######
+
+# x,y,z = 2,3,4
+# print(x,y,z)
+
+# i = j = z = h = "ornage"
+# x,y,z = 2,3,4
+# print(i,j,h,z)
+
+
+######## Outputs ########
+
+# with string we cannot concatenate other datatypes 
+
+x = "ornages"
+y = 5
+# print("I like " + x)
+
+print(x + str(y))
 
 
 
