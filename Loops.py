1839a9a701e4413cfa6d5934a3e2a3979eb9c002
khuara17 <vishalkamlapure.be@gmail.com>
2021-06-18 13:42:02 +0530

    18 June -- If-else, Loops

---
+++ Loops.py
@@ -0,0 +1,130 @@
+########### If else ###############
+
+'''
+if condition:
+    statement
+else:
+    statement
+    '''
+
+# print(4>3)
+# if "H" in "hello":
+#     print("4 is greater")
+# else:
+#     print("4 is not greater")
+
+### elif = else if #############
+'''
+if condition:
+    statement
+elif condition:
+    statement
+else:
+    statement
+    '''
+# t = 56
+
+# if t < 56:
+#     print("part 1")
+# elif t <= 56:
+#     print("part 2.5")
+# elif t == 56:
+#     print("part 2")
+# else:
+#     print("part 3")
+
+# print("I am out of if")
+
+################### for loops ##################
+
+''' for '''
+
+#### print numbers from 1 to 50 ##########
+
+# i = 1 + 1 = 2 + 1 = 3
+
+## Assign the value to the variable
+## Check the condition 
+## Execute whatever present in for loop
+## Increment the value by whatever defined in for loop
+
+## The conition in for loop is  "<" ,,, not <=
+
+# i = 1
+# i < 50
+# print(1)
+# i = 1 + 10 =11
+# 11 < 50
+# i = 11 + 10 =21
+
+
+# i = 1
+# 1 < 3
+# print
+
+# i = 2
+# i < 3
+# print
+
+# i = 2 + 1 = 3
+# i = 3 < 3
+
+
+'''
+for variable in range(start,endValue,Increment amout):'''
+
+# for i in range(1,5,1):
+#     print(i)
+
+
+## Default value :  start value == 0 and increment value == 1
+for i in range(5):
+    if i == 2:
+        break
+    print(i)
+
+## Default value : increment value == 1
+# for i in range(1,6): 
+#     print(i)
+
+
+# i = 5
+# i  = 5 > 1
+# print(5)
+# i = 5 - 1 = 4
+# 4 > 1
+# print(4)
+# i = 4 -1 = 3
+# 3 > 1
+# print
+# i = 3 - 1 =2
+# print
+# i = 2-1
+# 1 > 1
+
+
+# for i in range5(5,0,-1):
+#     print(i)
+
+####### While Loop #####################
+
+''' 
+Initialize variable 
+Check th condition 
+if true: 
+    then go to while loop 
+    increment the variable
+'''
+
+i = 0
+while i < 4:
+    if i == 2:
+        break
+    print(i)
+    i = i + 1
+print("h")
+#### break
+    
+
+
+
